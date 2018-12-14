4890566172ac9200eaafeb80f779ababb1ea86e4
satoru196 <na.kamura.1263@gmail.com>
2018-12-15 01:30:00 +0900

    add iiif curation dialog

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -37,6 +37,19 @@ function footer(rootDir){
   });
 }
 
+function curation(rootDir){
+  $.ajax({
+    url: rootDir + "include/curation.html", // ディレクトリー変更
+    cache: false,
+    async: false,
+    dataType: 'html',
+    success: function(html){
+      html = html.replace(/\{\$root\}/g, rootDir);
+      document.write(html);
+    }
+  });
+}
+
 function check(rootDir){
   $.ajax({
     url: rootDir + "include/check.html", // ディレクトリー変更
@@ -54,6 +67,25 @@ function show_modal(link, value){
   $("#link").attr("href", link)
   $("#value").text(value)
   $('#myModal').modal()
+}
+
+function show_curation_modal(curation){
+  $.ajax({
+    url:curation
+  })
+  .then(
+    function (result) {
+      $("#modal-title").text(result.label)
+      var description = result["selections"][0]["members"][0]["description"]
+      if(description){
+        var e = $(description)
+        $("#description").empty().append(e)
+      }
+      $("#iframe").attr("src", "iiif-curation-player/embed.html?curation="+curation)
+      $("#link").attr("href", "iiif-curation-player/?curation="+curation)
+      $('#curationModal').modal()
+    }
+  );
 }
 
 $(document).ready(function() {
