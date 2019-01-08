3336fa6ef11779e7fb9801b792c5905685489473
satoru196 <na.kamura.1263@gmail.com>
2019-01-08 11:55:19 +0900

    fix codes based on page speed insight

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -89,6 +89,26 @@ function show_curation_modal(curation){
   );
 }
 
+function show_thumbnail_modal(curation, thumbnail){
+  $("#modal_thumbnail").attr("src", thumbnail)
+  $.ajax({
+    url:curation
+  })
+  .then(
+    function (result) {
+      $("#modal-title").text(result.label)
+      var description = result["selections"][0]["members"][0]["description"]
+      if(description){
+        var e = $(description)
+        $("#description").empty().append(e)
+      }
+
+      $("#curationLink").attr("href", "iiif-curation-player/?curation="+curation)
+      $('#curationModal').modal()
+    }
+  );
+}
+
 $(document).ready(function() {
   var pagetop = $('.pagetop');
   $(window).scroll(function () {
