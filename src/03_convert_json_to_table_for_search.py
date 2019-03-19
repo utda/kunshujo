9d36cc992a9c24f41e92056e8a893e59418402df
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-03-19 12:06:38 +0900

    fix bug

--- src/03_convert_json_to_table_for_search.py
+++ src/03_convert_json_to_table_for_search.py
@@ -24,6 +24,9 @@ foaf = "http://xmlns.com/foaf/0.1/"
 archiveshub = "http://data.archiveshub.ac.uk/def/"
 bibo = "http://purl.org/ontology/bibo/"
 
+# rep = "nakamura196.github.io/kunshujo"
+rep = "kunshujo.dl.itc.u-tokyo.ac.jp"
+
 # 表示用項目
 arr = [dcterms+"description", dcterms+"extent", dcterms+"date", dcterms+"coverage", dcterms+"type", dcterms+"format", dcterms+"subject"]
 
@@ -40,12 +43,13 @@ for i in range(len(df)):
 
     obj2.append("")
     if dcterms+"relation" in obj:
-        obj2.append('<a onclick="show_curation_modal(\''+obj[dcterms+"relation"][0]["@id"].replace("kunshujo.dl.itc.u-tokyo.ac.jp","nakamura196.github.io/kunshujo")+'\'); return false;"><span style="display : none;">true</span><img class="lazy z-depth-1" style="max-height:151px; max-width:200px;" data-src="'+obj[foaf+"thumbnail"][0]["@id"]+'"></a>')
+        obj2.append('<a onclick="show_curation_modal(\''+obj[dcterms+"relation"][0]["@id"].replace("kunshujo.dl.itc.u-tokyo.ac.jp",rep)+'\'); return false;"><span style="display : none;">true</span><img class="lazy z-depth-1" style="max-height:151px; max-width:200px;" data-src="'+obj[foaf+"thumbnail"][0]["@id"]+'"></a>')
     else:
         obj2.append('')
 
     obj2.append(form(obj[dcterms+"title"][0]["@value"]))
-    obj2.append("『捃拾帖』第"+str(obj[bibo+"volume"][0]["@value"])+"帖")
+    volume = str(obj[bibo+"volume"][0]["@value"])
+    obj2.append('<span style="display : none;">'+volume.zfill(3)+"</span>『捃拾帖』第"+volume+"帖")
     obj2.append('<span style="display : none;">'+str(obj[archiveshub+"note"][0]["@value"])+'</span>'+obj[dcterms+"identifier"][0]["@value"])
     for e in arr:
         text = ""
@@ -64,7 +68,7 @@ for i in range(len(df)):
         obj2.append(form(text))
 
     if dcterms+"relation" in obj:
-        obj2.append('<a href="iiif-curation-player/?curation='+obj[dcterms+"relation"][0]["@id"].replace("kunshujo.dl.itc.u-tokyo.ac.jp","nakamura196.github.io/kunshujo")+'" target="cr"><img src="assets/images/icp-logo.png"></a>')
+        obj2.append('<a href="iiif-curation-player/?curation='+obj[dcterms+"relation"][0]["@id"].replace("kunshujo.dl.itc.u-tokyo.ac.jp",rep)+'" target="cr"><img src="assets/images/icp-logo.png"></a>')
     else:
         obj2.append('')
 
